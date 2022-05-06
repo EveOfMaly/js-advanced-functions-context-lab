4867cff6163f15fb8f93582d046dde9f63bdb88c
EveOfMaly <john.a.kyeremeh@gmail.com>
2022-05-05 21:13:19 -0400

    commit tree 4f074d5074d60ea4d4e42fad87570f0ec0dadc77

--- index.js
+++ index.js
@@ -9,6 +9,28 @@
  for you to use if you need it!
  */
 
+
+let createEmployeeRecord = function(row){
+   let employeeObject = {
+        firstName : row[0],
+        familyName : row[1],
+        title : row[2],
+        payPerHour : row[3],
+        timeInEvents : [],
+        timeOutEvents : []
+
+    }
+    return employeeObject
+}
+
+
+function createEmployeeRecords(employeeData){
+    return employeeData.map(row => createEmployeeRecord(row))
+}
+
+
+
+
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -20,3 +42,86 @@ let allWagesFor = function () {
 
     return payable
 }
+
+        
+function createTimeInEvent(timeIn){
+    let timeInConversion = timeIn.split(" ")
+    let date = timeInConversion[0]
+    let hour = timeInConversion[1]
+
+    let newTimeInObject = {}
+    newTimeInObject.type = "TimeIn"
+    newTimeInObject.date = date 
+    newTimeInObject.hour = parseInt(hour)
+
+    this.timeInEvents.push(newTimeInObject)
+
+    return this
+}
+
+
+
+function createTimeOutEvent(timeOut) {
+    
+    let timeOutConversion = timeOut.split(" ")
+    let date = timeOutConversion[0]
+    let hour = timeOutConversion[1]
+
+    
+
+    let newTimeOutObject = {}
+    newTimeOutObject.type = "TimeOut"
+    
+    newTimeOutObject.date = date 
+    newTimeOutObject.hour = parseInt(hour)
+    this.timeOutEvents.push(newTimeOutObject)
+    return this
+}
+
+
+
+
+
+function hoursWorkedOnDate(soughtDate){
+
+    let inEvent = this.timeInEvents.find(function(e){
+        return e.date === soughtDate
+    })
+
+    
+    let outEvent = this.timeOutEvents.find(function(e){
+        return e.date === soughtDate
+        debugger
+    })
+    
+
+    return (outEvent.hour - inEvent.hour) / 100
+}
+
+
+let wagesEarnedOnDate = function(dateSought){
+    let rawWage = hoursWorkedOnDate.call(this, dateSought)
+        * this.payPerHour
+    return parseFloat(rawWage.toString())
+}
+
+
+function findEmployeeByFirstName(employeeRecords, firstName) {
+
+    let employeefirstName = employeeRecords.find(function(e) {
+        return e.firstName === firstName
+    })
+    
+    return employeefirstName
+}
+
+function calculatePayroll(employeeRecords) {
+
+    
+    return employeeRecords.reduce(function(memo, record){
+    
+        return memo + allWagesFor.call(record)
+        
+    }, 0)
+}
+
